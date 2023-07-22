434480013b6512c7c5a5cbc5db9eb57ca3008d8e
Goddo-ro <r0manovic1@yandex.ru>
2023-07-22 17:40:21 +0300

    Add fetching markdowns from firestore

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,15 +4,34 @@ import {
   DrawerCloseButton,
   DrawerHeader,
   DrawerBody,
-  DrawerContent, DrawerFooter, Button, Flex, Text, Spacer, IconButton
+  DrawerContent, Flex, Text, Spacer
 } from '@chakra-ui/react'
 import { useAuth } from "../hooks/useAuth";
 import { AiOutlineFileAdd } from "react-icons/ai";
-import React from "react";
-
+import { getDocs, collection} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 
 const Sidebar = ({ isOpen, onClose }) => {
+  const [markdowns, setMarkdowns] = useState([]);
+
   const { email } = useAuth();
+
+  const markdownsCollectionRef = collection(db, "docs");
+
+  const fetchMarkdowns = async () => {
+    try {
+      const data = await getDocs(markdownsCollectionRef);
+      const filteredData = data.docs.map((doc) => ({...doc.data()}));
+      setMarkdowns(filteredData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchMarkdowns();
+  }, []);
 
   return (
     <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
